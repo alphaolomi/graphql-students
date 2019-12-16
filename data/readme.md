f486b9b965b3b7bd26b7f7f6a6baf7c3ce5b1157
Alpha Olomi <alphaolomi@gmail.com>
2019-12-16 09:48:36 +0300

    feat: init

---
+++ data/readme.md
@@ -0,0 +1,19 @@
+
+http://localhost:9000/graphiql
+
+
+request
+```json
+{
+Test
+}
+```
+
+Response
+```json
+{
+"data": {
+"test": "Test Success, GraphQL server is running !!"
+}
+}
+```
---
+++ server.js
@@ -0,0 +1,21 @@
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const express = require("express");
+
+const db = require("./db");
+
+const app = express();
+const fs = require("fs");
+const typeDefs = fs.readFileSync("./schema.graphql", { encoding: "utf-8" });
+const resolvers = require("./resolvers");
+
+const { makeExecutableSchema } = require("graphql-tools");
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+app.use(cors(), bodyParser.json());
+
+const { graphiqlExpress, graphqlExpress } = require("apollo-server-express");
+app.use("/graphql", graphqlExpress({ schema }));
+app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
+
+const port = process.env.PORT || 9000;
+app.listen(port, () => console.info(`Server started on port ${port}`));

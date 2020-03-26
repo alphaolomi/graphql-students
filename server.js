3adf3232d20c7a4a7016f551c47e42b0ea9a5727
Alpha Olomi <alphaolomi@gmail.com>
2020-03-26 11:47:39 +0300

    feat: added index to react app

--- server.js
+++ server.js
@@ -5,18 +5,33 @@ const express = require("express");
 const db = require("./db");
 
 const app = express();
+app.use(cors(), bodyParser.json());
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.get('*', (req, res) => {
+        res.sendfile(path.join(__dirname = 'client/build/index.html'));
+    })
+}
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/public/index.html'));
+});
+
 const fs = require("fs");
-const typeDefs = fs.readFileSync("./schema.graphql", { encoding: "utf-8" });
+const typeDefs = fs.readFileSync("./schema.graphql", {encoding: "utf-8"});
 const resolvers = require("./resolvers");
 
-const { makeExecutableSchema } = require("graphql-tools");
-const schema = makeExecutableSchema({ typeDefs, resolvers });
-app.use(cors(), bodyParser.json());
+//GRaphQL
+const {makeExecutableSchema} = require("graphql-tools");
+const schema = makeExecutableSchema({typeDefs, resolvers});
 
-const { graphiqlExpress, graphqlExpress } = require("apollo-server-express");
 
-app.use("/graphql", graphqlExpress({ schema }));
-app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
+const {graphiqlExpress, graphqlExpress} = require("apollo-server-express");
+
+app.use("/graphql", graphqlExpress({schema}));
+app.use("/graphiql", graphiqlExpress({endpointURL: "/graphql"}));
 
 const port = process.env.PORT || 9000;
 app.listen(port, () => console.info(`Server started on port ${port}`));

e1196a4e4dc23e9e1408ce947245bf562205ee52
Alpha Olomi <alphaolomi@gmail.com>
2020-03-26 12:35:32 +0300

    added more componetns

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import App from './App';
 import {ApolloClient} from 'apollo-client';
 import {InMemoryCache} from 'apollo-cache-inmemory';
 import {HttpLink} from 'apollo-link-http';
+import { ApolloProvider } from '@apollo/react-hooks';
 
 const BASE_URL = 'https://graphql-pokemon.now.sh/';
 
--- server.js
+++ server.js
@@ -1,37 +1,30 @@
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const express = require("express");
-
-const db = require("./db");
+const path = require('path');
 
 const app = express();
 app.use(cors(), bodyParser.json());
+
 app.use(express.static(path.join(__dirname, 'client/build')));
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    app.get('*', (req, res) => {
-        res.sendfile(path.join(__dirname = 'client/build/index.html'));
-    })
-}
-
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/public/index.html'));
 });
 
-const fs = require("fs");
-const typeDefs = fs.readFileSync("./schema.graphql", {encoding: "utf-8"});
-const resolvers = require("./resolvers");
+// const db = require("./db");
+// const fs = require("fs");
+// const typeDefs = fs.readFileSync("./schema/schema.graphql", {encoding: "utf-8"});
+// const resolvers = require("./resolvers");
+//
+// //GRaphQL
+// const {makeExecutableSchema} = require("graphql-tools");
+// const schema = makeExecutableSchema({typeDefs, resolvers});
+//
+//
+// const {graphiqlExpress, graphqlExpress} = require("apollo-server-express");
+//
+// app.use("/graphql", graphqlExpress({schema}));
+// app.use("/graphiql", graphiqlExpress({endpointURL: "/graphql"}));
 
-//GRaphQL
-const {makeExecutableSchema} = require("graphql-tools");
-const schema = makeExecutableSchema({typeDefs, resolvers});
-
-
-const {graphiqlExpress, graphqlExpress} = require("apollo-server-express");
-
-app.use("/graphql", graphqlExpress({schema}));
-app.use("/graphiql", graphiqlExpress({endpointURL: "/graphql"}));
-
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 9001;
 app.listen(port, () => console.info(`Server started on port ${port}`));

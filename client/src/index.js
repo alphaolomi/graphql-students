b126541416f28ea29f5d5976f643e118ac0d15cc
Alpha Olomi <alphaolomi@gmail.com>
2020-03-26 12:04:18 +0300

    feat: create Apollo Client

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import {ApolloClient} from 'apollo-client';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import {HttpLink} from 'apollo-link-http';
+
+const BASE_URL = 'https://graphql-pokemon.now.sh/'
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+    uri: BASE_URL
+})
+
+const client = new ApolloClient({
+    cache,
+    link
+})
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 

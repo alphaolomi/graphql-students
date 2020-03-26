85e43ee9a0d471ff46e1a49442a9241c16f7aeee
Alpha Olomi <alphaolomi@gmail.com>
2020-03-26 12:35:20 +0300

    feat: update

--- client/src/index.js
+++ client/src/index.js
@@ -7,21 +7,21 @@ import {ApolloClient} from 'apollo-client';
 import {InMemoryCache} from 'apollo-cache-inmemory';
 import {HttpLink} from 'apollo-link-http';
 
-const BASE_URL = 'https://graphql-pokemon.now.sh/'
+const BASE_URL = 'https://graphql-pokemon.now.sh/';
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
     uri: BASE_URL
-})
+});
 
 const client = new ApolloClient({
     cache,
     link
-})
+});
 
 ReactDOM.render(
     <React.StrictMode>
-        <App/>
+        <ApolloProvider client={client}><App/></ApolloProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
